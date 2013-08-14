dc50577154ea11e8759481dd8e560d43c81ca62f
0x24a537r9 <0x24a537r9@gmail.com>
2013-08-13 23:08:25 -0700

    Cleaned up urls.py and views.py.

--- davincist/urls.py
+++ davincist/urls.py
@@ -20,45 +20,30 @@ BADGE_GROUP = r'(?P<badge_id>%s)' % BADGE_REGEX
 USER_REGEX = r'[^\s/]{1,30}'
 USER_GROUP = r'(?P<username>%s)' % USER_REGEX
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
+    # Third-party pages.
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('social_auth.urls')),
 
+    # Top-level pages.
     url(r'^(?i)$', home, name='home'),
     url(r'^(?i)about/$', about, name='about'),
 
     # Track pages.
-    # url(r'^(?i)t/add$' % TRACK_GROUP, track_add, name='track_add'),
     url(r'^(?i)t/all$', track_list, name='track_list'),
     url(r'^(?i)t/%s/$' % TRACK_GROUP, track_detail, name='track_detail'),
     url(r'^(?i)t/%s/users/$' % TRACK_GROUP, track_users, name='track_users'),
-    # url(r'^(?i)t/%s/leave$' % TRACK_GROUP, track_leave, name='track_leave'),
-    # url(r'^(?i)t/%s/gallery$' % TRACK_GROUP, track_gallery, name='track_gallery'),
-
     url(r'^(?i)t/%s/levels/$' % TRACK_GROUP, track_levels, name='track_levels'),
-    # url(r'^(?i)t/%s/level/add$' % TRACK_GROUP, level_add, name='level_add'),
-    # url(r'^(?i)t/%s/level/%s$' % (TRACK_GROUP, LEVEL_GROUP), level_detail, name='level_detail'),
-    # url(r'^(?i)t/%s/level/%s/edit$' % (TRACK_GROUP, LEVEL_GROUP), level_edit, name='level_edit'),
-    # url(r'^(?i)t/%s/level/%s/delete$' % (TRACK_GROUP, LEVEL_GROUP), level_delete, name='level_delete'),
-
-    #url(r'^(?i)t/%s/quests/verify/$' % TRACK_GROUP, quests_verify, name='quests_verify'),
-    # url(r'^(?i)t/%s/quest/add$' % TRACK_GROUP, quest_add, name='quest_add'),
-    url(r'^(?i)t/%s/badge/%s/$' % (TRACK_GROUP, BADGE_GROUP), badge_detail, name='badge_detail'),
-    # url(r'^(?i)t/%s/quest/%s/edit$' % (TRACK_GROUP, QUEST_GROUP), quest_edit, name='quest_edit'),
-    # url(r'^(?i)t/%s/quest/%s/delete$' % (TRACK_GROUP, QUEST_GROUP), quest_delete, name='quest_delete'),
-
     url(r'^(?i)t/%s/badges/$' % TRACK_GROUP, track_badges, name='track_badges'),
-    # url(r'^(?i)t/%s/badge/%s$' % (TRACK_GROUP, BADGE_GROUP), badge_detail, name='badge_detail'),
+    url(r'^(?i)t/%s/badge/%s/$' % (TRACK_GROUP, BADGE_GROUP), badge_detail, name='badge_detail'),
 
     # User pages.
     url(r'^(?i)u/signup/$', user_add, name='user_add'),
-    # url(r'^(?i)u/all$', user_list, name='user_list'),
     url(r'^(?i)u/%s/$' % USER_GROUP, user_home, name='user_home'),
     url(r'^(?i)u/%s/merits/(?:%s/)?' % (USER_GROUP, TRACK_GROUP), user_merits, name='user_merits'),
-    url(r'^(?i)u/%s/gallery/$' % USER_GROUP, user_gallery, name='user_gallery'),
     url(r'^(?i)u/%s/edit/' % USER_GROUP, user_edit, name='user_edit'),
-    # url(r'^(?i)u/%s/delete' % USER_GROUP, user_delete, name='user_delete'),
 
     # AJAX pages.
     url(r'^(?i)x/get-wall-posts/', ajax_get_wall_posts, name='ajax_get_wall_posts'),
@@ -67,11 +52,9 @@ urlpatterns = patterns('',
     url(r'^(?i)x/complete-unverified-badge/', ajax_complete_unverified_badge, name='ajax_complete_unverified_badge'),
     url(r'^(?i)x/join-track/', ajax_join_track, name='ajax_join_track'),
 
-
+    # Authentication flow pages.
     url(r'^login-error/$', login_error, name='login_error'),
     url(r'^form/$', form, name='form'),
     url(r'^form2/$', form2, name='form2'),
     url(r'^close_login_popup/$', close_login_popup, name='login_popup_close'),
-
-
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
