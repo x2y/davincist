b78a3d5e96a7a4e151670992a3eac39f9ab1a3cc
0x24a537r9 <0x24a537r9@gmail.com>
2013-09-07 00:59:10 -0700

    Implements a queued-message banner used for communicating user events.

--- static/base.js
+++ static/base.js
@@ -1,3 +1,39 @@
+var activeBannerIndex = 0;
+var bannerMessages = [];
+var isShowingBanner = false;
+
+function showBannerMessage(message) {
+  bannerMessages.push(message);
+  if (!isShowingBanner) {
+    rotateBanner();
+  }
+}
+
+function rotateBanner() {
+  var $banners = $('.banner');
+  $banners.eq(activeBannerIndex).fadeOut();
+  activeBannerIndex = 1 - activeBannerIndex;
+
+  isShowingBanner = bannerMessages.length > 0;
+  if (isShowingBanner) {
+    var message = bannerMessages[0];
+    bannerMessages = bannerMessages.slice(1);
+
+    var $activeBanner = $banners.eq(activeBannerIndex);
+    var $activeBannerText = $activeBanner.find('.banner-text');
+    $activeBannerText.text(message);
+    $activeBanner.fadeIn();
+    $activeBannerText.textFit({
+      alignVert: true,
+      alignHoriz: true,
+      multiLine: true,
+      reProcess: true,
+      maxFontSize: 120
+    });
+    setTimeout(rotateBanner, 2500);
+  }
+}
+
 function trim(s) {
   return s.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 }
